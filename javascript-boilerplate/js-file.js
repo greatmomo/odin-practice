93d69a522f91108aa62ef8fa248b9fea6bea42f7
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-11-29 18:52:56 +0100

    multiple listeners at once

--- javascript-boilerplate/js-file.js
+++ javascript-boilerplate/js-file.js
@@ -1,4 +1,4 @@
-// DOM Manipulation
+// DOM Manipulation - Practice next
 
 const container = document.querySelector('#container');
 
@@ -39,5 +39,19 @@ function alertFunction() {
 // button listener/onclick
 const btn = document.querySelector('#btn');
 //btn.onclick = alertFunction;
-btn.addEventListener('click', alertFunction);
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
 //btn.onclick = () => alert("Hello World!");
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
